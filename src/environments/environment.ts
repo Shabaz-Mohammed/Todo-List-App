63b81ce5d3f0daacd85645f583b76b45cece4eda
Shabaz-Mohammed <shabazui786@gmail.com>
2019-09-07 15:39:00 +0530

    first commit

---
+++ src/environments/environment.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  production: false,
+  firebase: {
+    apiKey: 'AIzaSyBCz2bz0Lg5XctagcMoTjKicv_EYy2MDd0',
+    authDomain: 'todolistapp-524f6.firebaseapp.com',
+    databaseURL: 'https://todolistapp-524f6.firebaseio.com',
+    projectId: 'todolistapp-524f6',
+    storageBucket: '',
+    messagingSenderId: '457305835487',
+    appId: '1:457305835487:web:855c5d689c3639ba'
+  }
+};
